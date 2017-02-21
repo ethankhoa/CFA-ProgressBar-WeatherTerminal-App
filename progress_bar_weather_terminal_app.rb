c0885d5457d476af91c962cb9becb4dbff466701
Ethan Huynh <Ethan@E-and-R-MacBook-Pro.local>
2017-02-21 13:48:05 +1100

    adding v2 into devel branch

--- progress_bar_weather_terminal_app.rb
+++ progress_bar_weather_terminal_app.rb
@@ -1,64 +1,119 @@
-# Progress bar
+# Progress bar v2
+# Updating survey functionality
+#   convert answers array data into new array
+#   print both arrays into table
+#   paint numbers into colors based on number if
+# Improve progress bar functionality
+# Display answers array in table format
 
-# incrementally update
+
+
+# terminal-table class
+require 'terminal-table'
+# paint class
+require 'paint'
+
 
 # class for the Progress_bar
 # What does it do? What does it need?
-#
 
 class Progress_bar
-  def initialize(title) # what does it need
+  def initialize(title, total_actions) # what does it need
     @title = title
-    @current_actions = 1
-    # commenting below out because not needed yet
-    #@actions_remaining = @total_actions - @current_actions
+    @current_actions = 1.to_i
+    @total_actions = total_actions.to_i
   end
-  attr_accessor :title, :current_actions
-
-  def count_remaining # count how many remaining out of total
-    puts "Progress #{@current_actions}/#{@total_actions}"
-  end
-
-  def percent_remaining # percentage of how many remaining out of total
-    # needs to percent format
-    puts "Progress #{@current_actions/@total_actions*100}"
-  end
-
-  def action_complete
-    @current_actions = @current_actions + 1
-  end
+  attr_accessor :title, :current_actions, :total_actions
 
 end
 
-######### Begin Survey class
+# Begin Survey class
 class Survey
-  def initialize(questions) # what does it need
+  def initialize(questions, instructions) # what does it need
     @questions  = questions
+    @instructions = instructions
   end
-  attr_accessor :questions
+  attr_accessor :questions, :instructions
 
 # method to ask survey questions
   def run_survey(progress_bar)
     answers = [] # setting answers variable into array
+    converted_answers =[] # setting array for converted numbers
+
     #reference questions .each looks over entire array
-    # increment current step for every question in array
+    #increment current step for every question in array
     @questions.each do |question| # |signular of what is in array|
       system("clear") # clears out what was in there before
         # print progress bar, survey title: current questions / total questions
-        puts "#{progress_bar.title}: #{progress_bar.current_actions}/#{questions.length}"
+        puts "#{progress_bar.title}"
+        puts
+        puts "You are on question #{progress_bar.current_actions}/#{questions.length}"
+        puts
+        puts "#{instructions}"
+        puts
         puts question # singular of questions , ask user question in array
-        answer = gets.chomp # store answer from question
-        answers << answer # add answer into the end of the answers array
+        puts
+        answer = gets.chomp.to_i # store answer from question
+        #if answer.is_a?(Integer) # error check
+        # input values for Celcius
+          if answer >= 30 # setting criteria to paint value, if value greater or equal to 30
+          answers << Paint[answer, :red, :bold] # paint answer value bold red and input into answers array
+          else
+            answers << Paint[answer, :green] # any other value paint green
+          end
+          # input values for Fahrenheit
+          converted_answer = answer * 1.8 + 32 # convert answer value to Fahrenheit put in new variable
+          if converted_answer >= 85 # if value greater than or equal 85
+            converted_answers << Paint[converted_answer.to_i, :red, :bold] # if true paint bold red
+          else
+            converted_answers << Paint[converted_answer.to_i, :green] # any other value paint green
+          end # end of if
+        #  else # error check
+        #puts "oops" #error check
+   end
+
+
         #increment current_action for each answered question
         progress_bar.current_actions = progress_bar.current_actions + 1
-    end
+    end # end do for @questions
 
-
-    puts "Thanks for taking the weather survey. Here are your answers below."
+    puts "#" * 43
+    puts
+    puts Paint["Thanks for taking the survey. Here are your results.", :bold]
+    puts
       answers.each do |answer|
-        puts answer # print each answer from array individually
-      end
-    end
+      #  puts answer # print each answer from array individually
+  #    if answer >= 85
+  #      converted_answers << Paint[answer, :red]
+  #    else
+  #      converted_answers << answer
+  #    end
+    end # end do
+      #puts answers.inspect
+      #puts converted_answers.inspect
+
+
+
+    # begin table
+    # array for 7 days of the week
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    # combine arrays into 2d array
+    rows = [days, answers, converted_answers].transpose
+      #answers.each do |answer|
+      # rows << [answer]
+    #  end
+    #  converted_answers.each do |converted_answer|
+    #  rows << [converted_answer]
+    #  end
+
+
+    table = Terminal::Table.new :title => Paint[progress_bar.title, :blue], :headings => ['Day','Temp C', 'Temp F'], :rows => rows
+    table.style = {:all_separators => true}
+
+    puts Paint[table]
+
+end # end run_survey
+
 
 end # end of Survey class
 
@@ -87,11 +142,15 @@ questions_list = [ # array for questions
 ]
 # end questions list
 
+# begin instructions strings
+survey_instructions = "Please enter the temperature in celcius."
+
+
 
 # create survey and pass questions array into args
-survey_1 = Survey.new(questions_list)
+survey_1 = Survey.new(questions_list, survey_instructions)
 # create progress par object and pass in survey data
-progress_bar_1 = Progress_bar.new("Was it Hot or Not? I mean the weather!")
+progress_bar_1 = Progress_bar.new("Was it Hot or Not? I meant the weather!", 7)
 
 
 
